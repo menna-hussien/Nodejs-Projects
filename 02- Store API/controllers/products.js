83572731b17eeea9f34acdd9ca1c874eb1b0a51d
Menna Hussien <31513435+menna-hussien@users.noreply.github.com>
2022-05-12 17:17:57 +0200

    adding filtering and sorting features

--- 02- Store API/controllers/products.js
+++ 02- Store API/controllers/products.js
@@ -10,8 +10,39 @@ const getAllProducts = async (req, res) => {
 };
 
 const getfeaturedProducts = async (req, res) => {
-  const products = await Product.find(req.query);
-  console.log(req.query);
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  console.log(queryObject);
+
+  //sorting feature
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortedList = sort.split(',').join(' ');
+    result = result.sort(sortedList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  //search for certain fields
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  //pagination or limiting the results returned
+
+  //await the result then send the response
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
