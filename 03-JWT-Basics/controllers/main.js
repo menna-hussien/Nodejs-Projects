d1e5b38e2e77a5526c0c93995011d7d13df63664
Menna Hussien <31513435+menna-hussien@users.noreply.github.com>
2022-05-13 16:09:38 +0200

    add customized errors

--- 03-JWT-Basics/controllers/main.js
+++ 03-JWT-Basics/controllers/main.js
@@ -18,26 +18,11 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('no token provided ', 401);
-  }
-
-  const token = authHeader.split(' ')[1];
-  console.log(token);
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `hello, ${decoded.username}`,
-      secret: `here is ur authorized data, ur lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError('not authorized to access this route', 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `hello, ${req.user.username}`,
+    secret: `here is ur authorized data, ur lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
---
+++ 03-JWT-Basics/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const authenticationMiddleWare = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('no token provided ');
+  }
+
+  const token = authHeader.split(' ')[1];
+  console.log(token);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('not authorized to access this route');
+  }
+};
+
+module.exports = authenticationMiddleWare;
